4f3a60a3a2c76009fac486296a00a4e6a22be675
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-09-18 17:58:12 +0200

    provisoire

--- go/go.sh
+++ go/go.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf src/go1.23.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.1.linux-amd64.tar.gz
 
 export PATH="$PATH:/usr/local/go/bin"
 
