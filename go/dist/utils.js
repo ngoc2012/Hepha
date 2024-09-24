33995cff1255a0a297425fadf07e44a05ec16ea7
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-09-24 23:00:01 +0200

    tempory push

--- go/dist/utils.js
+++ go/dist/utils.js
@@ -1,4 +1,6 @@
-export const $ = s => document.querySelector(s)
-export const $a = s => document.querySelectorAll(s)
+export const $ = s => {
+    const e = document.querySelector(s)
+    if (!e) throw new Error(`Element not found: ${s}`)
+}
 export const ready = c => document.addEventListener('DOMContentLoaded', c)
 export const onClick = (e, c) => e.addEventListener('click', c)
