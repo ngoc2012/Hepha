8774b75751334ad071f704f3617d8022b882fdfe
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-09-24 23:12:02 +0200

    tempory push

--- go/dist/main.js
+++ go/dist/main.js
@@ -1,16 +1,10 @@
 import { $, ready, onClick } from "/utils.js";
 
 ready(() => {
-    let buttons = $('.main.contenst')
-    if (buttons instanceof NodeList) {
-        buttons.forEach(button => {
-            onClick(button, () => {
-                console.log('clicked')
-            });
+    let buttons = $('.main.content')
+    buttons.forEach(button => {
+        onClick(button, () => {
+            console.log('clicked')
         });
-    } else {
-        onClick(buttons, () => {
-        console.log('clicked')
-        });
-    }
+    });
 });
--- go/src/main.js
+++ go/src/main.js
@@ -2,15 +2,9 @@ import { $, ready, onClick } from "/utils.js";
 
 ready(() => {
     let buttons = $('.main.content')
-    if (buttons instanceof NodeList) {
-        buttons.forEach(button => {
-            onClick(button, () => {
-                console.log('clicked')
-            });
+    buttons.forEach(button => {
+        onClick(button, () => {
+            console.log('clicked')
         });
-    } else {
-        onClick(buttons, () => {
-        console.log('clicked')
-        });
-    }
+    });
 });
