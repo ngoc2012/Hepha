40c29f17bd16f3d668d6bfaca85de9ec1917a959
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-09-24 14:26:01 +0200

    tempory push

--- go/build-tool/main.go
+++ go/build-tool/main.go
@@ -21,20 +21,14 @@ type contentButon struct {
 
 func main() {
 	// Create a new instance of the app
-	buton := contentButon{
-		Number: 1,
-		Html: component.Render2Html("src/Buton.html", buton)
-	}
+	buton := contentButon{ Number: 1 }
+	buton.Html = component.Render2Html("src/Buton.html", buton),
 	print(buton.Html)
 
-	content := appContent{
-		Buton: buton,
-		Html: component.Render2Html("src/Content.html", content)
-	}
+	content := appContent{ Buton: buton }
+	Content.Html = component.Render2Html("src/Content.html", content),
 
 	print(content.Html)
-	index := app{
-		Content: content,
-		component.Render2File("src/layout.html", "dist/index.html", index)
-	}
+	index := app{ Content: content }
+	component.Render2File("src/layout.html", "dist/index.html", index),
 }
