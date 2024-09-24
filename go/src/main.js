5238502d2ff0c341b061f29f7fd6cec0c32af1ee
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-09-24 23:16:02 +0200

    tempory push

--- go/src/main.js
+++ go/src/main.js
@@ -1,5 +1,20 @@
 import { $, ready, onClick } from "/utils.js";
 
+class Button {
+    constructor(element) {
+        this.element = element;
+        this.state = 'default'; // Initial state
+    }
+
+    setState(newState) {
+        this.state = newState;
+    }
+
+    getState() {
+        return this.state;
+    }
+}
+
 ready(() => {
     let buttons = $('.main.content')
     buttons.forEach(button => {
