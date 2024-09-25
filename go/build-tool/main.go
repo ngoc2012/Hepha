b039ede21f592b78a62df4600ae3f5be0aacb71e
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-09-25 17:16:01 +0200

    tempory push

--- go/build-tool/main.go
+++ go/build-tool/main.go
@@ -10,18 +10,20 @@ type page struct {
 	Content pageContent
 }
 
-type pageContent struct {
+type examplesContent struct {
 	Html   template.HTML
 	Button contentButton
 }
 
-type contentButton struct {
+type examplesButton struct {
 	Html   template.HTML
 	Number int
 }
 
 func main() {
 	file.Clean()
+
+	content := pageContent{Html: component.FileContent2Html("src/Content.html")}
 
 	index := page{Content: content}
 	component.Render2File("src/layout.html", "dist/index.html", index)
@@ -32,7 +34,6 @@ func main() {
 	content = pageContent{Button: button}
 	content.Html = component.FileContent2Html("src/Content.html")
 
-	content := pageContent{Html: button}
 	examples_index := page{Content: content}
 	component.Render2File("src/examples/layout.html", "dist/examples/index.html", examples_index)
 }
