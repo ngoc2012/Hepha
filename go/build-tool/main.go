113ba3681c1fb2243042dc82921e70309cdcd96d
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-09-25 18:06:01 +0200

    tempory push

--- go/build-tool/main.go
+++ go/build-tool/main.go
@@ -14,8 +14,15 @@ func main() {
 	button := component.Render2Html("src/examples/Button.html", map[string]interface{}{
 		"Number": 1,
 	})
+
+	// Create a slice of buttons
+	buttons := make([]template.HTML, 10)
+	for i := 0; i < 10; i++ {
+		buttons[i] = button
+	}
+
 	content := component.Render2Html("src/examples/Content.html", map[string]interface{}{
-		"Button": template.HTML(button),
+		"Buttons": buttons,
 	})
 
 	component.Render2File("src/layout.html", "dist/examples/index.html", content)
