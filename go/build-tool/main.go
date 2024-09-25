ec8caf719b53760185437a51fbbf942b50036712
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-09-25 17:40:01 +0200

    tempory push

--- go/build-tool/main.go
+++ go/build-tool/main.go
@@ -3,37 +3,17 @@ package main
 import (
 	"build/component"
 	"build/file"
-	"html/template"
 )
-
-type page struct {
-	Html template.HTML
-}
-
-type examplesContent struct {
-	Html   template.HTML
-	Button examplesButton
-}
-
-type examplesButton struct {
-	Html   template.HTML
-	Number int
-}
 
 func main() {
 	file.Clean()
 
-	main_index := page{
-		Html: component.FileContent2Html("src/Content.html"),
-	}
-	component.Render2File("src/layout.html", "dist/index.html", main_index)
+	component.Render2File("src/layout.html", "dist/index.html", component.FileContent2Html("src/Content.html"))
 
-	button := examplesButton{Number: 1}
-	button.Html = component.Render2Html("src/Button.html", button)
+	button := component.Render2Html("src/examples/Button.html", 1)
 
-	content = pageContent{Button: button}
-	content.Html = component.FileContent2Html("src/Content.html")
+	content := component.Render2Html("src/examples/Content.html")
 
 	examples_index := page{Content: content}
-	component.Render2File("src/examples/layout.html", "dist/examples/index.html", examples_index)
+	component.Render2File("src/layout.html", "dist/examples/index.html", examples_index)
 }
