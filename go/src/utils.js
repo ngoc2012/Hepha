29b2d4d266fa0d99678274d7603ac782ec5b11c2
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-09-25 18:26:02 +0200

    tempory push

--- go/src/utils.js
+++ go/src/utils.js
@@ -1,11 +1,3 @@
 export const $ = s => document.querySelectorAll(s)
 export const ready = c => document.addEventListener('DOMContentLoaded', c)
 export const onClick = (e, c) => e.addEventListener('click', c)
-
-// If you need to return a single element, you can use the following code:
-// export const $ = s => {
-//     const e = document.querySelectorAll(s)
-//     if (e.length === 0) throw new Error(`Element not found: ${s}`)
-//     if (e.length === 1) return e[0]
-//     return e
-// }
