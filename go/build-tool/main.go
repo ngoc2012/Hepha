9244d9f9160fab691bbcb2f169f9653139df7993
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-09-26 13:44:01 +0200

    tempory push

--- go/build-tool/main.go
+++ go/build-tool/main.go
@@ -9,39 +9,39 @@ import (
 type Map map[string]interface{}
 
 func react_page() {
-	button := component.Render2Html("src/examples/react/Button.html", map[string]interface{}{"Number": 1})
+	button := component.Render2Html("src/examples/react/Button.html", Map{"Number": 1})
 
 	var buttons [10]template.HTML
 	for i := 0; i < 10; i++ {
 		buttons[i] = button
 	}
 
-	content := component.Render2Html("src/examples/react/Content.html", map[string]interface{}{"Buttons": buttons})
+	content := component.Render2Html("src/examples/react/Content.html", Map{"Buttons": buttons})
 
-	component.Render2File("dist/examples/react/index.html", "src/layout.html", map[string]interface{}{"Content": content})
+	component.Render2File("dist/examples/react/index.html", "src/layout.html", Map{"Content": content})
 }
 
 func remix_page() {
-	button := component.Render2Html("src/examples/react/Button.html", map[string]interface{}{"Number": 1})
+	button := component.Render2Html("src/examples/react/Button.html", Map{"Number": 1})
 
 	var buttons [10]template.HTML
 	for i := 0; i < 10; i++ {
 		buttons[i] = button
 	}
 
-	content := component.Render2Html("src/examples/react/Content.html", map[string]interface{}{"Buttons": buttons})
+	content := component.Render2Html("src/examples/react/Content.html", Map{"Buttons": buttons})
 
-	component.Render2File("dist/examples/react/index.html", "src/layout.html", map[string]interface{}{"Content": content})
+	component.Render2File("dist/examples/react/index.html", "src/layout.html", Map{"Content": content})
 }
 
 func main() {
 	file.Clean()
 
-	component.Render2File("dist/index.html", "src/layout.html", map[string]interface{}{
+	component.Render2File("dist/index.html", "src/layout.html", Map{
 		"Content": component.FileContent2Html("src/Content.html"),
 		"Style": ["/main.css", "/fonts/inter.css"],
 	})
-	component.Render2File("dist/examples/index.html", "src/layout.html", map[string]interface{}{"Content": component.FileContent2Html("src/examples/Content.html")})
+	component.Render2File("dist/examples/index.html", "src/layout.html", Map{"Content": component.FileContent2Html("src/examples/Content.html")})
 
 	react_page()
 	remix_page()
