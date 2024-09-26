6746f24e12bce7efa7295be92d47d3a6828e665b
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-09-26 13:34:02 +0200

    tempory push

--- go/build-tool/main.go
+++ go/build-tool/main.go
@@ -7,16 +7,29 @@ import (
 )
 
 func react_page() {
-	button := component.Render2Html("src/examples/react/Button.html", map[string]interface{}{ "Number": 1, })
+	button := component.Render2Html("src/examples/react/Button.html", map[string]interface{}{"Number": 1})
 
 	var buttons [10]template.HTML
 	for i := 0; i < 10; i++ {
 		buttons[i] = button
 	}
 
-	content := component.Render2Html("src/examples/react/Content.html", map[string]interface{}{ "Buttons": buttons, })
+	content := component.Render2Html("src/examples/react/Content.html", map[string]interface{}{"Buttons": buttons})
 
-	component.Render2File("src/layout.html", "dist/examples/react/index.html", content)
+	component.Render2File("dist/examples/react/index.html", "src/layout.html", content)
+}
+
+func remix_page() {
+	button := component.Render2Html("src/examples/react/Button.html", map[string]interface{}{"Number": 1})
+
+	var buttons [10]template.HTML
+	for i := 0; i < 10; i++ {
+		buttons[i] = button
+	}
+
+	content := component.Render2Html("src/examples/react/Content.html", map[string]interface{}{"Buttons": buttons})
+
+	component.Render2File("dist/examples/react/index.html", "src/layout.html", content)
 }
 
 func main() {
@@ -26,4 +39,5 @@ func main() {
 	component.Render2File("dist/examples/index.html", "src/layout.html", component.FileContent2Html("src/examples/Content.html"))
 
 	react_page()
+	remix_page()
 }
