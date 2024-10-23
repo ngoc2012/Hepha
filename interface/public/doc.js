7adcd1a4a3ad81ee5eaea51c4e46dfee87c5d11d
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-10-23 09:54:02 +0200

    tempory push

--- interface/public/doc.js
+++ interface/public/doc.js
@@ -1,19 +1,21 @@
 function toggleMode() {
-  document.documentElement.setAttribute('data-theme', document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark');
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  document.documentElement.setAttribute('data-theme', newTheme);
 
   const toggleIcon = document.getElementById('toggle-icon');
     if (newTheme === 'dark') {
-        toggleIcon.src = 'dark-mode-icon.svg';
+        toggleIcon.src = '/sun.svg';
     } else {
-        toggleIcon.src = 'light-mode-icon.svg';
+        toggleIcon.src = '/moon.svg';
     }
 }
 
 // Apply dark mode class if user prefers dark mode
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.documentElement.setAttribute('data-theme', 'dark');
-  document.getElementById('toggle-icon').src = 'dark-mode-icon.svg';
+  document.getElementById('toggle-icon').src = '/sun.svg';
 } else {
   document.documentElement.setAttribute('data-theme', 'light');
-  document.getElementById('toggle-icon').src = 'light-mode-icon.svg';
+  document.getElementById('toggle-icon').src = '/moon.svg';
 }
