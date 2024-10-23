0088a5bd5794f340f42057eca6b84917f4d388b5
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-10-23 17:42:01 +0200

    tempory push

--- interface/public/doc.js
+++ interface/public/doc.js
@@ -1,3 +1,5 @@
+import { $, ready, onClick } from "/selector.js";
+
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.documentElement.setAttribute('data-theme', 'dark');
   setModeIcon('dark');
