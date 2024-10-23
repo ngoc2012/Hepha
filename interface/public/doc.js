2d4761dea493a5f5c478938d0c9a0350c529e25d
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-10-23 08:54:02 +0200

    tempory push

--- interface/public/doc.js
+++ interface/public/doc.js
@@ -5,6 +5,7 @@ function toggleMode() {
 // Apply dark mode class if user prefers dark mode
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.documentElement.setAttribute('data-theme', 'dark');
+  
 } else {
   document.documentElement.setAttribute('data-theme', 'light');
 }
