a1cb2ffb073f70407ff7bc4a9e143bd7f80d4c73
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-10-23 09:52:01 +0200

    tempory push

--- interface/public/doc.js
+++ interface/public/doc.js
@@ -1,11 +1,19 @@
 function toggleMode() {
   document.documentElement.setAttribute('data-theme', document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark');
+
+  const toggleIcon = document.getElementById('toggle-icon');
+    if (newTheme === 'dark') {
+        toggleIcon.src = 'dark-mode-icon.svg';
+    } else {
+        toggleIcon.src = 'light-mode-icon.svg';
+    }
 }
 
 // Apply dark mode class if user prefers dark mode
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.documentElement.setAttribute('data-theme', 'dark');
-  
+  document.getElementById('toggle-icon').src = 'dark-mode-icon.svg';
 } else {
   document.documentElement.setAttribute('data-theme', 'light');
+  document.getElementById('toggle-icon').src = 'light-mode-icon.svg';
 }
