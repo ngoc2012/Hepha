35805fde8fd1785434338d0c4efe58f7ae94951b
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-10-26 05:22:02 +0200

    tempory push

--- interface/public/selector.js
+++ interface/public/selector.js
@@ -1,3 +1,3 @@
-export const $ = s => Array.from(document.querySelectorAll(s))
+export const $ = s => document.querySelectorAll(s)
 export const ready = c => document.addEventListener('DOMContentLoaded', c)
 export const onClick = (e, c) => e.addEventListener('click', c)
