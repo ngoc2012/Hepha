5bc5a12a376fe24a774423e686b9cb35131ca55a
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-10-26 05:44:01 +0200

    tempory push

--- interface/public/doc.js
+++ interface/public/doc.js
@@ -36,11 +36,11 @@ console.log($(".page *"));
 var currentPage = $(".page")[0];
 $(".page *").forEach(e => {
   console.log(e.nodeType, getBottom(e), getBottom(currentPage));
-  if (getBottom(e) > getBottom(currentPage)) {
-    console.log("new page");
-    currentPage.insertAdjacentHTML("afterend", '<div class="page"></div>');
-    currentPage = currentPage.nextElementSibling;
-    currentPage.appendChild(e);
-  }
-  
+  // if (getBottom(e) > getBottom(currentPage)) {
+  //   console.log("new page");
+  //   currentPage.insertAdjacentHTML("afterend", '<div class="page"></div>');
+  //   currentPage = currentPage.nextElementSibling;
+  //   currentPage.appendChild(e);
+  // }
+  currentPage.appendChild(e);
 });
