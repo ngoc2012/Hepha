6e35b38d6d1d8c38124147c5c7a3cbe9193c65b6
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-10-30 16:48:01 +0100

    tempory push

--- interface/public/doc.js
+++ interface/public/doc.js
@@ -47,6 +47,7 @@ ready(() => {
         pages = $(".page");
       }
       for (let j = iE; j < elementsPage.length; j++) {
+        console.log(j)
         elementsPage[j] = iP;
         pages[iP].appendChild(e);
       }
