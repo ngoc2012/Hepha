da17fe7d148eed575b5dd85e6fc0d305ff7d9ef2
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-11-02 13:28:02 +0100

    tempory push

--- interface/public/doc.js
+++ interface/public/doc.js
@@ -51,15 +51,19 @@ ready(() => {
     }
   });
 
-  // trigger extension
-  ace.require("ace/ext/language_tools");
+  // // trigger extension
+  // ace.require("ace/ext/language_tools");
+  // var editor = ace.edit("editor");
+  // editor.session.setMode("ace/mode/html");
+  // editor.setTheme("ace/theme/tomorrow");
+  // // enable autocompletion and snippets
+  // editor.setOptions({
+  //     enableBasicAutocompletion: true,
+  //     enableSnippets: true,
+  //     enableLiveAutocompletion: false
+  // });
+
   var editor = ace.edit("editor");
-  editor.session.setMode("ace/mode/html");
-  editor.setTheme("ace/theme/tomorrow");
-  // enable autocompletion and snippets
-  editor.setOptions({
-      enableBasicAutocompletion: true,
-      enableSnippets: true,
-      enableLiveAutocompletion: false
-  });
+  editor.setTheme("ace/theme/twilight");
+  editor.session.setMode("ace/mode/javascript");
 });
