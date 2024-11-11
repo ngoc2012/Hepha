e9e73a3797f03caa208f0451b8fdee479525dd78
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-11-11 19:56:01 +0100

    tempory push

---
+++ html-template-babel/server.ts
@@ -0,0 +1,16 @@
+// server.ts
+import fs from 'fs';
+import path from 'path';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import App from './dist/App';
+
+const htmlTemplate = fs.readFileSync(path.resolve(__dirname, 'src/index.html'), 'utf8');
+
+const appHtml = ReactDOMServer.renderToString(React.createElement(App));
+
+const finalHtml = htmlTemplate.replace('<!-- APP -->', appHtml);
+
+fs.writeFileSync(path.resolve(__dirname, 'dist/index.html'), finalHtml);
+
+console.log('Static HTML generated successfully!');
