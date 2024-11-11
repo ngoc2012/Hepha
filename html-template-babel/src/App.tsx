a49565087db548456380f7ee56111536a721e94a
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-11-11 18:22:01 +0100

    tempory push

---
+++ html-template-babel/src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
