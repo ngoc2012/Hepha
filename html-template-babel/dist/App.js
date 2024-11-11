7b7a864b9eaefff3799137012dfcaeb71ac42968
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-11-11 18:26:01 +0100

    tempory push

---
+++ html-template-babel/dist/App.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+// src/App.tsx
+
+var App = function App() {
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h1", null, "Hello, World!"));
+};
+_reactDom["default"].render(/*#__PURE__*/_react["default"].createElement(App, null), document.getElementById('root'));
