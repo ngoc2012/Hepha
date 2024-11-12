db571a28134371cf43a895fc4c98c0a4b68e2426
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-11-12 11:18:01 +0100

    tempory push

--- html-template-babel/dist/App.js
+++ html-template-babel/dist/App.js
@@ -1,11 +1,14 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
 var _react = _interopRequireDefault(require("react"));
-var _reactDom = _interopRequireDefault(require("react-dom"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 // src/App.tsx
 
 var App = function App() {
   return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h1", null, "Hello, World!"));
 };
-_reactDom["default"].render(/*#__PURE__*/_react["default"].createElement(App, null), document.getElementById('root'));
+var _default = exports["default"] = App;
---
+++ html-template-babel/dist/server.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var _fs = _interopRequireDefault(require("fs"));
+var _path = _interopRequireDefault(require("path"));
+var _react = _interopRequireDefault(require("react"));
+var _server = _interopRequireDefault(require("react-dom/server"));
+var _App = _interopRequireDefault(require("./App"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+// server.ts
+
+var htmlTemplate = _fs["default"].readFileSync(_path["default"].resolve(__dirname, 'src/index.html'), 'utf8');
+var appHtml = _server["default"].renderToString(/*#__PURE__*/_react["default"].createElement(_App["default"]));
+var finalHtml = htmlTemplate.replace('<!-- APP -->', appHtml);
+_fs["default"].writeFileSync(_path["default"].resolve(__dirname, 'dist/index.html'), finalHtml);
+console.log('Static HTML generated successfully!');
